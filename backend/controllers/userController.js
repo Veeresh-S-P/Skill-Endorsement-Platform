955600864c1c09bf478c74c4be8d99cf149fd68a
Veeresh S P <112792652+Veeresh-S-P@users.noreply.github.com>
2025-05-17 11:12:21 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,7 @@ exports.getUserProfile = async (req, res) => {
 };
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find(); // Assuming MongoDB
+    const users = await User.find(); 
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
